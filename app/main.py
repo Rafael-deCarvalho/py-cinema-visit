6285cb30f0acd270b41c6ce11c702998000b48bf
Rafael Carvalho <dannyelrc@hotmail.com>
2026-06-01 12:25:49 -0300

    Solution - 4

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ from app.people import customer, cinema_staff
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
 
     for customer in customers:
-        sell_product(customer["food"], customer["name"])
+        CinemaBar.sell_product(customer["food"], customer["name"])
 
-    movie_session(movie, customers, cleaner)
+    CinemaHall.movie_session(movie, customers, cleaner)
